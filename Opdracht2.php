cf07790496da0fb91befe416a1a40c5f5ae44b45
Fayaaz Nabie <fnabie@roc-dev.com>
2019-09-23 11:50:30 +0200

    Opdracht 2

---
+++ Opdracht2.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Fayaaz Nabie
+ * Date: 23-9-2019
+ * Time: 11:48
+ */
+
+$boodschap = "aardappel";
+
+echo "Ik moet vandaag ". $boodschap ."en kopen.";

0832456565bbc06304d0991e53012dfc29ca055d
Fayaaz Nabie <fnabie@roc-dev.com>
2019-09-24 15:16:22 +0200

    PHP Connect

--- db_connnection.php
+++ db_connnection.php
@@ -1,7 +1,17 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Fayaaz Nabie
- * Date: 24-9-2019
- * Time: 15:11
- */
+function OpenCon()
+{
+    $dbhost = "localhost";
+    $dbuser = "fayaaznabie";
+    $dbpass = "fayaaz160501";
+    $db = "challenge2019";
+    $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+
+    return $conn;
+}
+
+function CloseCon($conn)
+{
+    $conn -> close();
+}
+
